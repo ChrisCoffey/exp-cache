4f8718645ac5641a8a0bc1c072896a198206f7d2
Chris Coffey <cmcof273@gmail.com>
2018-03-13 08:37:49 -0400

    Basic tests

--- test/Spec/LRUTests.hs
+++ test/Spec/LRUTests.hs
@@ -26,6 +26,24 @@ seqLRUTests = testGroup "Sequential" [
             s = recordLookup (1 :: Int) strat
             (s', Just _) = evict s :: (SeqLRU Int, Maybe Int)
         s' @=? strat
-
+    , testCase "read a b a => evict b a" $ do
+        let strat = newSeqLRU :: SeqLRU Int
+            [x,y] = [1,2] :: [Int]
+            s = recordLookup x . recordLookup y $ recordLookup x strat
+            (s', Just a) = evict s
+            (s'', Just b) = evict s'
+            (_, Nothing) = evict s'' :: (SeqLRU Int, Maybe Int)
+        a == y @=? True
+        b == x @=? True
+    , testCase "read a b c => evict a b c" $ do
+        let strat = newSeqLRU :: SeqLRU Int
+            [x,y,z] = [1,2,3] :: [Int]
+            s = recordLookup x . recordLookup y $ recordLookup z strat
+            (s', Just a) = evict s
+            (s'', Just b) = evict s'
+            (_, Just c) = evict s'' :: (SeqLRU Int, Maybe Int)
+        a @=? x
+        b @=? y
+        c @=? z
     ]
 
