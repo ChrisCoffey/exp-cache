afb029ce53375e736b4715e97fbed612fc4865ce
Chris Coffey <cmcof273@gmail.com>
2018-04-09 08:56:33 -0400

    Wire into Travis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lru-cache
+
+[![Build Status](https://travis-ci.org/ChrisCoffey/Cache-Eviction-Strategies.svg?branch=master)](https://travis-ci.org/ChrisCoffey/Cache-Eviction-Strategies)
--- test/Spec/RRTests.hs
+++ test/Spec/RRTests.hs
@@ -21,12 +21,28 @@ rrSpec = testGroup "RR Spec" [
             let rr = newRR defaultGen 1000
                 rr' = insertN rr 5000
             1000 @=? rrSizeDebug rr'
-        ],
-    testCase "empty strategy eviction is a no-op" $ do
+        ]
+    , testCase "empty strategy eviction is a no-op" $ do
         let rr = newRR defaultGen 10 :: RR Int
             (rr', res) = evict rr
         res @=? Nothing
         rr' @=? rr
+    , testCase "evict returns last evicted" $ do
+        let rr = newRR defaultGen 10 :: RR Int
+            rr' = insertN rr 1000
+            (_, evicted) = evict rr'
+        Just 5 @=? evicted
+    , testCase "evict is deterministic given a seed" $ do
+        let rr = newRR defaultGen 10 :: RR Int
+            rr' = insertN rr 1000
+            (e1, evicted) = evict rr'
+            e2 = recordLookup 2000 e1
+            (e3, evicted') = evict e2
+            e4 = recordLookup 10000 e3
+            (_, evicted'') = evict e4
+        Just 5 @=? evicted
+        Just 7 @=? evicted'
+        Just 10 @=? evicted''
     ]
 
 
